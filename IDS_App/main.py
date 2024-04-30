c903396f723ad928fe06822341ec2a2a2622981a
Leo Roth <leoroth123@yahoo.com>
2024-04-30 14:01:25 -0700

    adds scan results file to desired directory

--- IDS_App/main.py
+++ IDS_App/main.py
@@ -1,9 +1,28 @@
+import os
 import process_checker
 
 
-def add_to_file(output):  # currently functions as 'replace file' but finished product is 'add to file'
-    with open("output.txt", "w") as output_file:
-        output_file.write(output)
+def create_directory(dir_name, parent_dir_path):  # creates directory if one does not already exist
+    directory = dir_name
+    parent_dir = parent_dir_path
+    path = os.path.join(parent_dir, directory)
+    if not os.path.exists(path):
+        os.mkdir(path)
+    return path
+
+
+def create_file(output, dir_name, parent_dir_path):  # creates file for scan results
+    folder_path = create_directory(dir_name, parent_dir_path)
+    output_file = "output.txt"
+    file_path = os.path.join(folder_path, output_file)
+    i = 0
+    while os.path.exists(file_path):  # in the case of duplicate output file names
+        i += 1
+        file_name, file_extension = os.path.splitext(output_file)
+        output_file = f"{file_name}{i}{file_extension}"
+        file_path = os.path.join(folder_path, output_file)
+    with open(file_path, "w") as directory:
+        directory.write(output)
 
 
 def main():
@@ -16,7 +35,11 @@ def main():
 
     checker_result = process.check_running()
 
-    add_to_file(checker_result)
+    dir_name = input("\nName of desired directory for scan results: ")
+
+    parent_dir_path = input("\nParent directory path: ")
+
+    create_file(checker_result, dir_name, parent_dir_path)
 
     print("\n--Process Check Complete--\n")
 
